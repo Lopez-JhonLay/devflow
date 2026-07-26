af2a3e45dc3422054f790ddca952ffc68ab83b3a
Lopez-JhonLay <jhonlaylopez000@gmail.com>
2026-07-26 11:45:48 +0800

    chore: setup postgres and prisma connection with better auth meta tables

---
+++ server/src/auth/auth.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+// 1. Create the adapter with your connection string
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL as string,
+});
+
+// 2. Pass the adapter to the PrismaClient
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+});
